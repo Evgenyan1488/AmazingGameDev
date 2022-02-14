31e272fa5ffcfc26f99a9bf3c69d17f9ebcaf3c9
Evgenyan1488 <Shvedov.zheka@yandex.ru>
2022-02-15 00:17:16 +0300

    U suka

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -2,13 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun : MonoBehaviour
+public class Gun : Weapon
 {
+    public float bspeed;
+    public float bdistance;
+    public float lifetime;
 
-    public float offset = -90f;
+    public int dmg;
+    public LayerMask whatisSolid;
+
 
     public GameObject bullet;
     public Transform shotPoint;
+    
+    public Gun(float bspeed, float bdistance, float lifetime, int dmg)
+    {
+        this.bspeed = bspeed;
+        this.bdistance = bdistance;
+        this.lifetime = lifetime;
+        this.dmg = dmg;
+    }
 
     private float timebtwshots;
     public float atackspeed;
@@ -19,23 +32,24 @@ public class Gun : MonoBehaviour
     }
 
 
-
     void Update()
     {
-        Vector3 dif = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float rotZ = Mathf.Atan2(dif.y, dif.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
-        if (timebtwshots <= 0)
+        RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, transform.up, bdistance, whatisSolid);
+        if (hitinfo.collider != null)
         {
-            if (Input.GetMouseButton(0))
+            if(hitinfo.collider.CompareTag("Enemy"))
             {
-                Instantiate(bullet, shotPoint.position, transform.rotation);
-                timebtwshots = atackspeed;
+                hitinfo.collider.GetComponent<Enemy>().GetDamage(dmg);
             }
+            Destroy(gameObject);
         }
-        else
-        {
-            timebtwshots -= Time.deltaTime;
-        }
+        transform.Translate(Vector2.right * bspeed * Time.deltaTime);
+        StartCoroutine(LifeTimeDestruction());
+    }
+
+    private IEnumerator LifeTimeDestruction()
+    {
+        yield return new WaitForSeconds(lifetime);
+        Destroy(gameObject);
     }
 }
