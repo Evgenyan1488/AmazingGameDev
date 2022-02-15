81fc55cbeef37b8ca723a583178d899b512a7468
Evgenyan1488 <Shvedov.zheka@yandex.ru>
2022-02-15 23:36:21 +0300

    Revert "Создание задатка великой игры"
    This reverts commit d8355efd3017b0346bae4d2a5a7de547aa8d31ed.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -2,28 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun : Weapon
+public class Gun : MonoBehaviour
 {
-    public float bspeed;
-    public float bdistance;
-    public float lifetime;
 
-    public int dmg;
-    public LayerMask whatisSolid;
+    public float offset = -90f;
 
-<<<<<<< Updated upstream
     public GameObject bullet;
     public Transform shotPoint;
     
-=======
-    public Gun(float bspeed, float bdistance, float lifetime, int dmg)
-    {
-        this.bspeed = bspeed;
-        this.bdistance = bdistance;
-        this.lifetime = lifetime;
-        this.dmg = dmg;
-    }
->>>>>>> Stashed changes
 
     void Start()
     {
@@ -31,9 +17,9 @@ public class Gun : Weapon
     }
 
 
+
     void Update()
     {
-<<<<<<< Updated upstream
         Vector3 dif = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotZ = Mathf.Atan2(dif.y, dif.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
@@ -42,24 +28,5 @@ public class Gun : Weapon
         {
             Instantiate(bullet, shotPoint.position, transform.rotation);
         }
-=======
-        RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, transform.up, bdistance, whatisSolid);
-        if (hitinfo.collider != null)
-        {
-            if(hitinfo.collider.CompareTag("Enemy"))
-            {
-                hitinfo.collider.GetComponent<Enemy>().GetDamage(dmg);
-            }
-            Destroy(gameObject);
-        }
-        transform.Translate(Vector2.right * bspeed * Time.deltaTime);
-        StartCoroutine(LifeTimeDestruction());
-    }
-
-    private IEnumerator LifeTimeDestruction()
-    {
-        yield return new WaitForSeconds(lifetime);
-        Destroy(gameObject);
->>>>>>> Stashed changes
     }
 }
